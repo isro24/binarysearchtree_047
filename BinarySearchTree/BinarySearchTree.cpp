b928699f38af38dee00048ab9b2774cd01da6e4b
isro24 <isro.usman.ft22@mail.umy.ac.id>
2023-07-02 19:25:30 +0700

    Menambahkan kode untuk memperbarui nilai parent dan currentNode dalam setiap iterasi perulangan.

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -62,6 +62,12 @@ public:
 		currentNode = ROOT;
 		parent = NULL;
 		while ((currentNode != NULL) && (currentNode->info != element))
-
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
 	}
 };
